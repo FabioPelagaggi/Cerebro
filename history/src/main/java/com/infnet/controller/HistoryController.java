0aa0b749e14ee5b5975575f243e6ad34e903b52d
Fabio Pelagaggi <fabio.pnunes@al.infnet.edu.br>
2024-09-15 16:36:22 -0300

    Removing unused imports

--- history/src/main/java/com/infnet/controller/HistoryController.java
+++ history/src/main/java/com/infnet/controller/HistoryController.java
@@ -1,9 +1,6 @@
 package com.infnet.controller;
 
-import java.util.List;
-
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
