3dd7cd0266f569b3c5342430668ba86d1b2d3d02
Fabio Pelagaggi <fabio.pnunes@al.infnet.edu.br>
2024-09-15 14:15:15 -0300

    Exemples added for Mutant

--- mutant/src/main/java/com/infnet/controller/MutantController.java
+++ mutant/src/main/java/com/infnet/controller/MutantController.java
@@ -17,6 +17,7 @@ import com.infnet.service.MutantService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -34,10 +35,24 @@ public class MutantController {
     @Operation(summary = "Register a mutant", description = "Check if is a Mutant and register in the database")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Mutant registered successfully", content = {
-                    @Content(mediaType = "application/json", schema = @Schema(implementation = MutantModel.class))
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = MutantModel.class), examples = @ExampleObject(value = "{\n"
+                            +
+                            "  \"name\": \"Jean Grey\",\n" +
+                            "  \"realName\": \"Jean Grey\",\n" +
+                            "  \"level\": \"Omega\",\n" +
+                            "  \"mutantPowers\": [\n" +
+                            "    \"Telepathy\",\n" +
+                            "    \"Telekinesis\",\n" +
+                            "    \"Phoenix Force\"\n" +
+                            "  ],\n" +
+                            "  \"description\": \"Jean Grey is a powerful mutant, one of the founding members of the X-Men. She possesses telepathic and telekinetic abilities. Jean is also the host of the Phoenix Force, making her one of the most powerful beings in the universe.\",\n"
+                            +
+                            "  \"image\": \"https://upload.wikimedia.org/wikipedia/en/thumb/7/7c/Marvel_Girl_House_of_X.jpg/220px-Marvel_Girl_House_of_X.jpg\"\n"
+                            +
+                            "}"))
             }),
-            @ApiResponse(responseCode = "400", description = "Bad request, the subject is not a mutant"),
-            @ApiResponse(responseCode = "500", description = "Internal server error")
+            @ApiResponse(responseCode = "400", description = "Bad request, the subject is not a mutant", content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
     })
     @PostMapping
     public ResponseEntity<?> registerMutant(@RequestBody MutantRegistrationRequest mutantRegistrationRequest) {
@@ -54,15 +69,15 @@ public class MutantController {
     @Operation(summary = "Get a mutant", description = "Get a mutant by its ID")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Mutant found successfully", content = {
-                    @Content(mediaType = "application/json", schema = @Schema(implementation = MutantModel.class))
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = MutantModel.class), examples = @ExampleObject(value = "1"))
             }),
-            @ApiResponse(responseCode = "404", description = "Mutant not found"),
-            @ApiResponse(responseCode = "500", description = "Internal server error")
+            @ApiResponse(responseCode = "404", description = "Mutant not found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
     })
     @GetMapping("{id}")
-    public ResponseEntity<?> getMutant(@PathVariable("id") Long id){
+    public ResponseEntity<?> getMutant(@PathVariable("id") Long id) {
         log.info("Getting mutant with id: {}", id);
-    
+
         try {
             return ResponseEntity.ok(mutantService.getMutant(id));
         } catch (Exception e) {
@@ -73,12 +88,12 @@ public class MutantController {
 
     @Operation(summary = "Delete a mutant", description = "Delete a mutant by its ID")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Mutant deleted successfully"),
-            @ApiResponse(responseCode = "404", description = "Mutant not found"),
-            @ApiResponse(responseCode = "500", description = "Internal server error")
+            @ApiResponse(responseCode = "200", description = "Mutant deleted successfully", content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class), examples = @ExampleObject(value = "1"))),
+            @ApiResponse(responseCode = "404", description = "Mutant not found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
     })
     @DeleteMapping("{id}")
-    public ResponseEntity<?> deleteMutant(@PathVariable("id") Long id){
+    public ResponseEntity<?> deleteMutant(@PathVariable("id") Long id) {
         log.info("Deleting mutant with id: {}", id);
         try {
             mutantService.deleteMutant(id);
@@ -88,15 +103,23 @@ public class MutantController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Mutant not found");
         }
     }
-    
+
     @Operation(summary = "Update a mutant", description = "Update a mutant by its ID")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Mutant updated successfully"),
-            @ApiResponse(responseCode = "404", description = "Mutant not found"),
-            @ApiResponse(responseCode = "500", description = "Internal server error")
-    })
+            @ApiResponse(responseCode = "200", description = "Mutant registered successfully", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = MutantModel.class), examples = @ExampleObject(value = "{\n"
+                            + "  \"name\": \"Wolverine\",\n" + "  \"realName\": \"James Howlett\",\n"
+                            + "  \"level\": \"Alpha\",\n" + "  \"mutantPowers\": [\n" + "    \"Regeneration\",\n"
+                            + "    \"Enhanced Senses\",\n" + "    \"Adamantium Claws\"\n" + "  ],\n"
+                            + "  \"description\": \"Wolverine, also known as Logan, is a mutant with powerful regenerative abilities, enhanced senses, and retractable adamantium claws. He is a member of the X-Men and has a mysterious and violent past.\",\n"
+                            + "  \"image\": \"https://upload.wikimedia.org/wikipedia/en/e/e3/Wolverine_%28Marvel_Character%29.png\"\n"
+                            + "}")) }),
+            @ApiResponse(responseCode = "404", description = "Mutant not found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))) })
+
     @PutMapping("{id}")
-    public ResponseEntity<?> updateMutant(@PathVariable("id") Long id, @RequestBody MutantRegistrationRequest mutantRegistrationRequest){
+    public ResponseEntity<?> updateMutant(@PathVariable("id") Long id,
+            @RequestBody MutantRegistrationRequest mutantRegistrationRequest) {
         log.info("Updating mutant with id: {}", id);
         try {
             mutantService.updateMutant(id, mutantRegistrationRequest);
@@ -112,11 +135,11 @@ public class MutantController {
             @ApiResponse(responseCode = "200", description = "All mutants found successfully", content = {
                     @Content(mediaType = "application/json", schema = @Schema(implementation = MutantModel[].class))
             }),
-            @ApiResponse(responseCode = "404", description = "Mutants not found"),
-            @ApiResponse(responseCode = "500", description = "Internal server error")
+            @ApiResponse(responseCode = "404", description = "Mutants not found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
     })
     @GetMapping("all")
-    public ResponseEntity<?> getAllMutants(){
+    public ResponseEntity<?> getAllMutants() {
         log.info("Getting all mutants");
         try {
             return ResponseEntity.ok(mutantService.getAllMutants());
